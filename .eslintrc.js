03fc276f189f706d17df9da7cfac9dee13ebef1e
Aleksandr_UB <bogdanov777000@mail.ru>
2023-07-21 13:46:06 +0300

    24 - add RTL tests for Sidebar, Button

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         browser: true,
         es2021: true,
         jest: true,
-
     },
     extends: [
         'plugin:react/recommended',
@@ -27,10 +26,13 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'no-unused-vars': 'off',
+        'no-unused-vars': 'warn',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
@@ -39,12 +41,24 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'react/prop-types': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { code: 180, ignoreComments: true }],
-
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['to', 'data-testid'],
+            },
+        ],
+        'max-len': ['error', { ignoreComments: true, code: 190 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
