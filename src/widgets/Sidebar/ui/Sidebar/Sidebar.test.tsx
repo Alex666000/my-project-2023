9a253defbc34045e922d1d03c40b089ce62a1e26
Aleksandr_UB <bogdanov777000@mail.ru>
2023-07-21 13:16:13 +0300

    24 - add RTL tests for Sidebar, Button

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,24 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
+import {
+    renderWithTranslation,
+} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+
+describe('Sidebar', () => {
+    test('with only first param', () => {
+        renderWithTranslation(<Sidebar />);
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+    });
+
+    // Для сворачивания и разворачивания Сайдбара
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar />);
+        // на кнопку в компоненте вешаем testid - находим кнопку
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        // кликаем по кнопке
+        fireEvent.click(toggleBtn);
+        // ожидаем что класс для сворачивания навешался на элемент
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+    });
+});
